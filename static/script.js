fdd2178835f759ed8e2131c0d81806a3642abfeb
rockwillck <38663064+rockwillck@users.noreply.github.com>
2023-01-28 23:52:38 -0600

    Fixed routing issue, got animation toggle working, etc.

--- static/script.js
+++ static/script.js
@@ -2,29 +2,55 @@ let swipe = document.createElement('div');
 swipe.id = "swipe"
 let nav = document.createElement('div');
 nav.classList.add('nav');
-tabs = {"Home":"index.html", "Test":"test.html"}
+
+tabs = {"Home":"index.html", "Projects":"projects.html", "Blog":"blog/blog.html", "Tutorials":"tutorials/tutorials.html"}
 nav.innerHTML = ""
+matched = false
+let headRoute = window.location.href.split("/").slice(0, 3).join("/")
+let file = window.location.href.split("/").slice(3, window.location.href.split("/").length).join("/")
 for ([n, src] of Object.entries(tabs)) {
     nav.innerHTML += `
-<button class="${window.location.toString().split("/")[window.location.toString().split("/").length - 1] == src ? 'selected' : 'link'}" onclick="route('${src}')">${n}</button>`
+<button class="${file == src ? 'selected' : 'link'}" onclick="route('${headRoute + "/" + src}')">${n}</button>`
+    if (window.location.toString().split("/")[window.location.toString().split("/").length - 1] == src.split("/")[src.split("/").length - 1]) {
+        title = document.createElement("title")
+        title.innerText = n
+        document.head.appendChild(title)
+        matched = true
+    }
 }
+nav.innerHTML += `<img class='options' src='${headRoute + "/static/anim.svg"}' onclick="localStorage.setItem('animation', localStorage.getItem('animation') == 'true' ? 'false' : 'true')"></img>`
+hiddenTabs = ["404.html"]
+if (!matched && !hiddenTabs.includes(window.location.toString().split("/")[window.location.toString().split("/").length - 1])) {
+    window.location.replace("index.html")
+}
+
+
 current = document.body.innerHTML
 document.body.innerHTML = ""
 document.body.appendChild(nav)
-document.body.appendChild(swipe)
 document.body.innerHTML += current + `
 <footer>
-    <small>© ${new Date().getFullYear()} William Choi-Kim. All Rights Reserved.</small></footer>
+    <small>© ${new Date().getFullYear()} William Choi-Kim. All Rights Reserved.</small>
 </footer>`
 
-setTimeout(() => {
-    document.getElementById("swipe").style.transition = "2s"
-    document.getElementById("swipe").style.left = "-100vw"
-}, 1)
+if (localStorage.getItem("animation") != null) {
+        document.body.appendChild(swipe)
+        setTimeout(() => {
+            document.getElementById("swipe").style.transition = `${localStorage.getItem("animation") == "true" ? 1 : 0}s`
+            document.getElementById("swipe").style.left = "-100vw"
+        }, 1)
+} else {
+    localStorage.setItem("animation", "true")
+}
 
 function route(but) {
-    document.getElementById("swipe").style.left = "0"
-    setTimeout(() => {
+    document.getElementById("swipe").style.transition = `${localStorage.getItem("animation") == "true" ? 1 : 0}s`
+    if (localStorage.getItem("animation") == "true") {
+        document.getElementById("swipe").style.left = "0"
+        setTimeout(() => {
+            window.location.href = but
+        }, 1200)
+    } else {
         window.location.href = but
-    }, 2500)
+    }
 }
